0eec3ae180aa0b54ffb16e8d60435d7f8ed0bb02
Kegen Guyll <guyllkegen@gmail.com>
2018-10-30 23:23:00 -0500

    added the firebase files

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default{
+    // Initialize Firebase
+    apiKey: "AIzaSyBaV0T8UJxhQ9S5mkWc4cN5dwO3DqHWXko",
+    authDomain: "todolist-4f7ad.firebaseapp.com",
+    databaseURL: "https://todolist-4f7ad.firebaseio.com",
+    projectId: "todolist-4f7ad",
+    storageBucket: "todolist-4f7ad.appspot.com",
+    messagingSenderId: "522254049328"
+}
