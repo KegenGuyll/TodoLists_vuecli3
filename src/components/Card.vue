9eb9cc335db612493828c71d517810982e145e37
Kegen Guyll <guyllkegen@gmail.com>
2018-10-30 23:17:11 -0500

    Please enter the commit message for your changes. Lines starting  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/components/Card.vue 	modified:   src/components/sidenav.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,118 +1,138 @@
 <template>
-   <div class="container">
-      <div class="row">
-    <div class="col s12">
-      <div class="card-panel">
-        <span class="white-text">
-            <div>
-                <div class="input-field col s12">
-                    <input  id="title" type="text" class="white-text" v-model="title">
-                    <label for="title">   Title</label>
+<div class="container">
+    <div class="row">
+        <div class="col s12">
+            <div class="card-panel">
+                <span class="white-text">
+                    <div>
+                        <div class="input-field col s12">
+                            <input id="title" type="text" class="white-text" v-model="title">
+                            <label for="title"> Title</label>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="input-field col s12">
+                            <textarea id="textarea2" class="materialize-textarea white-text" data-length="120" v-model="task"></textarea>
+                            <label for="textarea2"> Task</label>
+                        </div>
+                    </div>
+                </span>
+                <div style="text-algin right">
+                    <a class="waves-effect waves-light btn right-text" v-on:click="addTodo"><i class="material-icons right">send</i>Send</a>
                 </div>
-            </div>   
-            <div>
-                <div class="input-field col s12">
-                    <textarea id="textarea2" class="materialize-textarea white-text" data-length="120" v-model="task"></textarea>
-                    <label for="textarea2"> Task</label>
-                </div>
-            </div>     
-        </span>
-        <div style="text-algin right">
-            <a class="waves-effect waves-light btn right-text" v-on:click="addTodo"><i class="material-icons right">send</i>Send</a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class='col s4 m4 l4'>
-     <div v-for=" (todo, index) in todos" :key="todo.id" class="card">
-      <i  v-on:click="close(index)" class="material-icons right white-text">close</i>
-      <div class=" card-content white-text">
-          <div>
-              <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-title">
-                <span class="card-title"> {{todo.title}}</span>
-              </div>
-              <input v-else  class="todo-item-edit" type='text' 
-               v-model="todo.title" @blur="doneEdit(todo)">
-          </div>
-          <div>      
-            <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-task">
-                <p>{{todo.task}}</p>
             </div>
-            <input v-else  class="todo-item-edit" type='text' 
-            v-model="todo.task" @blur="doneEdit(todo)">
-          </div>     
-      </div>
-    </div> 
-  </div>
-</div>   
+        </div>
+    </div>
+    <div class='col s4 m4 l4'>
+        <div v-for=" (todo, index) in todos" v-bind:key='todo.id' class="card">
+            <i v-on:click="close(index,todo)" class="material-icons right white-text">close</i>
+            <div class=" card-content white-text">
+                <div>
+                    <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-title">
+                        <span class="card-title"> {{todo.title}}</span>
+                    </div>
+                    <input v-else class="todo-item-edit" type='text' v-model="todo.title" @blur="doneEdit(todo)">
+                </div>
+                <div>
+                    <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-task">
+                        <p>{{todo.task}}</p>
+                        <p style="color:rgba(71, 110, 118, 0.49); text-align: right;">{{todo.date}}</p>
+                    </div>
+                    <input v-else class="todo-item-edit" type='text' v-model="todo.task" @blur="doneEdit(todo)">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
+import db from './firebaseInit'
 export default {
-  name: 'card',
-  data () {
-      return{
-          title: '',
-          task: '',
-          idForTodo: 1,
-          date: '',
-          editing: false,
-          todos: [
-              
-          ]
-      }
-  },
-  methods:{
-      addTodo(){
-          var d = new Date();
-          if (this.title == ''){
-              this.title = 'Task' + this.idForTodo
-
-          
-            if (this.task == ''){
-                alert('You must have a task')
-            }
-            else{
-                this.todos.push({
-                    id: this.idForTodo,
-                    title: this.title,
-                    task: this.task,
-                    completed: false,
-                    editing: false,
-                    date: d.getMonth() +1 + '/' + d.getDate() + '/' + d.getFullYear()
-                    
-          })
-          this.idForTodo++
-        }
-    }
-    else{
-        
-        this.todos.push({
-            id: this.idForTodo,
-            title: this.title,
-            task: this.task,
-            completed: false,
+    name: 'card',
+    data() {
+        return {
+            title: '',
+            task: '',
+            idForTodo: 1,
+            date: '',
+            contentid: null,
             editing: false,
-            date: d.getMonth() +1 + '/' + d.getDate() + '/' + d.getFullYear()
-          })
-          this.idForTodo++
-    }
-          this.newTodo = ''
-          this.title = ''
-          this.task = ''
-      },
-      close(index){
-          this.todos.splice(index,1)
-      },
-      editTodo(todo){
-          todo.editing = true
-      },
-      doneEdit(todo){
-          todo.editing = false
-      },
+            todos: [
 
-      
-  }
+            ]
+        }
+        
+    },
+    created () {
+        db.collection('task').orderBy('date').get().then(querySnapshot =>{
+            querySnapshot.forEach(doc => {
+                const data = {
+                    title : doc.data().title,
+                    task : doc.data().task,
+                    date : doc.data().date,
+                    contentid : doc.data().contentid
+
+                }
+                this.todos.push(data)
+            })
+        })
+    },
+    methods: {
+        addTodo() {
+
+                var d = new Date();
+                var NewContentId = Math.floor((Math.random() * 100) + 1)
+                if (this.task == '') {
+                    alert("You must have a task")
+                } else {
+
+                    if (this.title == '') {
+                        this.title = 'Task ' + this.idForTodo
+                    }
+
+                    db.collection('task').add({
+                            title: this.title,
+                            task: this.task,
+                            completed: false,
+                            editing: false,
+                            date: d.getMonth() + 1 + '/' + d.getDate() + '/' + d.getFullYear(),
+                            contentid: NewContentId
+                        })
+                        
+                    this.todos.push({
+                        title: this.title,
+                        task: this.task,
+                        completed: false,
+                        editing: false,
+                        date: d.getMonth() + 1 + '/' + d.getDate() + '/' + d.getFullYear(),
+                        contentid: NewContentId
+                    })
+                    this.idForTodo++
+                    this.title = ''
+                    this.task = ''
+                }
+        },
+        close(index,todo) {
+             if ( confirm('Are you sure?')){
+                 db.collection('task').where('contentid', '==' , todo.contentid).get()
+                 .then(querySnapshot => {
+                     querySnapshot.forEach(doc =>{
+                         doc.ref.delete();
+                         this.todos.splice(index, 1)
+                     })
+                 })
+             }
+        },
+        editTodo(todo) {
+            todo.editing = true
+        },
+        doneEdit(todo) {
+            todo.editing = false
+        },
+
+
+    }
 }
 
 </script>
@@ -120,62 +140,87 @@ export default {
 <!--- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-.card-panel,.card{
+.card-panel,
+.card {
     background-color: #66b5c8;
     word-break: break-all;
 
 }
 
-.btn{
+.btn {
     background-color: #7dd0e6
-
 }
 
-p{
+p {
     word-break: break-all;
 }
 
-input:not([type]):focus:not([readonly]), input[type=text]:not(.browser-default):focus:not([readonly]), input[type=password]:not(.browser-default):focus:not([readonly]), input[type=email]:not(.browser-default):focus:not([readonly]), input[type=url]:not(.browser-default):focus:not([readonly]), input[type=time]:not(.browser-default):focus:not([readonly]), input[type=date]:not(.browser-default):focus:not([readonly]), input[type=datetime]:not(.browser-default):focus:not([readonly]), input[type=datetime-local]:not(.browser-default):focus:not([readonly]), input[type=tel]:not(.browser-default):focus:not([readonly]), input[type=number]:not(.browser-default):focus:not([readonly]), input[type=search]:not(.browser-default):focus:not([readonly]), textarea.materialize-textarea:focus:not([readonly]){
+input:not([type]):focus:not([readonly]),
+input[type=text]:not(.browser-default):focus:not([readonly]),
+input[type=password]:not(.browser-default):focus:not([readonly]),
+input[type=email]:not(.browser-default):focus:not([readonly]),
+input[type=url]:not(.browser-default):focus:not([readonly]),
+input[type=time]:not(.browser-default):focus:not([readonly]),
+input[type=date]:not(.browser-default):focus:not([readonly]),
+input[type=datetime]:not(.browser-default):focus:not([readonly]),
+input[type=datetime-local]:not(.browser-default):focus:not([readonly]),
+input[type=tel]:not(.browser-default):focus:not([readonly]),
+input[type=number]:not(.browser-default):focus:not([readonly]),
+input[type=search]:not(.browser-default):focus:not([readonly]),
+textarea.materialize-textarea:focus:not([readonly]) {
     border-bottom: 1px solid #ffffff;
     -webkit-box-shadow: 0 1px 0 0 #ffffff;
     box-shadow: 0 1px 0 0 #ffffff;
 }
-input:not([type]), input[type=text]:not(.browser-default), input[type=password]:not(.browser-default), input[type=email]:not(.browser-default), input[type=url]:not(.browser-default), input[type=time]:not(.browser-default), input[type=date]:not(.browser-default), input[type=datetime]:not(.browser-default), input[type=datetime-local]:not(.browser-default), input[type=tel]:not(.browser-default), input[type=number]:not(.browser-default), input[type=search]:not(.browser-default), textarea.materialize-textarea{
+
+input:not([type]),
+input[type=text]:not(.browser-default),
+input[type=password]:not(.browser-default),
+input[type=email]:not(.browser-default),
+input[type=url]:not(.browser-default),
+input[type=time]:not(.browser-default),
+input[type=date]:not(.browser-default),
+input[type=datetime]:not(.browser-default),
+input[type=datetime-local]:not(.browser-default),
+input[type=tel]:not(.browser-default),
+input[type=number]:not(.browser-default),
+input[type=search]:not(.browser-default),
+textarea.materialize-textarea {
     border-bottom: 1px solid #7dd0e6 !important;
     /* background-color: rgba(71, 110, 118, 0.18); */
 }
-    
-::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
-    color: #5f6d6d
-;
+
+::placeholder {
+    /* Chrome, Firefox, Opera, Safari 10.1+ */
+    color: #5f6d6d;
 }
 
-:-ms-input-placeholder { /* Internet Explorer 10-11 */
-    color: #b9dedc
-;
+:-ms-input-placeholder {
+    /* Internet Explorer 10-11 */
+    color: #b9dedc;
 }
 
-::-ms-input-placeholder { /* Microsoft Edge */
-    color: #b9dedc
-;
+::-ms-input-placeholder {
+    /* Microsoft Edge */
+    color: #b9dedc;
 }
 
-.material-icons{
+.material-icons {
     cursor: pointer;
 
 }
 
-.input-field input[type=text]:focus + label {
-     color: #ffffff;
+.input-field input[type=text]:focus+label {
+    color: #ffffff;
 }
 
-textarea.materialize-textarea:focus:not([readonly]) + label{
+textarea.materialize-textarea:focus:not([readonly])+label {
     color: #ffffff !important;
 }
 
 .input-field label {
-     color: #33606b;
-   }
+    color: #33606b;
+}
 </style>
 
 <!--
--- src/components/sidenav.vue
+++ src/components/sidenav.vue
@@ -1,32 +1,48 @@
 <template>
-    <div> 
+<div>
   <nav>
-   <a href="#" data-target="slide-out" class="sidenav-trigger left"><i class="material-icons">menu</i></a>
+    <div class="row">
+       <a href="#" data-target="slide-out" class="sidenav-trigger left"><i class="material-icons">menu</i></a>
+       <input id="textbox" type="text" v-model="item_search">
+    </div>
   </nav>
   <ul id="slide-out" class="sidenav">
-    
-
   </ul>
-    </div>
+</div>
 </template>
 
 <script>
-// import card from './components/Card.vue'
-
+// import db from './firebaseInit'
 export default {
   name: 'sidenav',
+  data(){
+    return{
+      item_search: ''
+    }
+  }
 }
 </script>
 
 <style scoped>
-
-nav{
+nav {
   background-color: #66b5c8 !important;
   color: #7dd0e6;
 }
 
-#slide-out{
+#slide-out {
   background-color: #476e76
+}
+
+#textbox{
+  background-color: #476e766e;
+    flex: auto;
+    height: 0%;
+    width: 50%;
+    border-radius: 10px;
+    align-content: center;
+    margin-left: 13%;
+    border-bottom: hidden;
+    color: #ffffff
 }
 </style>
 
